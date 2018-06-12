34908e4550d4c66b39d0114f27a1380503b1a991
Jake Covert <jcovert7@ford.com>
2018-06-11 21:26:53 -0400

    Initial commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Covert, Jake (J.) on 6/11/18.
+//  Copyright © 2018 Woodward Pet Sitting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func redButton(_ sender: Any)     { view.backgroundColor = .red }
+    @IBAction func orangeButton(_ sender: Any)  { view.backgroundColor = .orange }
+    @IBAction func yellowButton(_ sender: Any)  { view.backgroundColor = .yellow}
+    @IBAction func greenButton(_ sender: Any)   { view.backgroundColor = .green}
+    @IBAction func blueButton(_ sender: Any)    { view.backgroundColor = .blue}
+    @IBAction func violetButton(_ sender: Any)  { view.backgroundColor = .magenta }
+    
+    
+
+
+}
+
